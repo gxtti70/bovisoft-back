087617c131d122e4edc4e79cc643e46a57e0804f
Santi M  🐱‍👤🔱 <150541114+gxtti70@users.noreply.github.com>
2026-03-29 22:12:17 -0500

    Updated db.js to remove unnecessary comments.

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// Creamos el pool usando directamente la URL
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // ¡ESTO ES VITAL! Sin esto, Neon rechazará la conexión por seguridad.
+    rejectUnauthorized: false
   }
 });
 
